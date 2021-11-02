d916151a28bfd88e43168dfe6f8ec155af3aea13
123Seba <92675765+123Seba@users.noreply.github.com>
2021-11-02 19:55:32 +0100

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,93 +1,166 @@
-﻿#include <iostream>
+﻿
 
-using namespace std;
-
+#include <iostream>
+using namespace  std;
 int main()
 {
-	////	////ctrl+k+c-komenda kodu ctrl+k+u-odkomentowanie kodu
-	////	//int nr_albumu = 65573;
-	////	//string nr_albumu1 = "w65573";
-	////	//cout << "Nr albumu " << nr_albumu << endl;
-	////	//cout << "Nr albumu " << nr_albumu1 << endl;
-	////
-	////
-	////	int a, b;//deklaracja zmiennych
-	//////wczytywanie danych  z klawiatury
-	////	cout << "Podaj a :", cin >> a;
-	////	//cout <<"podaj b;:,cin>>b;
-	////
-	////	if (a%2==0) {
-	////		cout << "Liczba parzysta\n";
-	////		cout << "kolejna linia tekstu\n";
-	////	}
-	////
-	////	else {
-	////	}
-	////
-	////
-	////}
-	//// 
-	////int main()
-	////{
-	////	//
-	////	float a, b;//deklaracja zmiennych
-	////	//wczytywanie danych  z klawiatury
-	////	cout << "Podaj a :", cin >> a;
-	////	//cout <<"podaj b;:,cin>>b;
-	////
-	////	if (a == b) {
-	////		cout << "Liczby sa rowne\n";
-	////	}
-	////	else if (a > b)
-	////		cout << "a jest wieksze " << endl;
-	////	
-	////
-	////	else cout << "b jest wieksze\n";
-	////
-	////
-	////}
-	// 
+	//	// zad 1//
+	//	int i = 0, liczba;
+	//	cout << " podaj liczbe : ", cin >> liczba;
+	//
+	//	cout << " petla while : \ n";
+	//	while (i<liczba )
+	//	{
+	//		cout << i << " ";
+	//		i++;
+	//	}
+	//
+	//	i = 0;
+	//	cout << " \npetla while : \ n";
+	//	do
+	//	{
+	//		cout << i << " ";
+	//		i++;
+	//	} while (i<liczba);
+	//	cout << " \npetla for : \ n";
+	//	for (i = 0; i < liczba; i++)
+	//	{
+	//		cout << i << " ";
+	//
+	//
+	//}
+	//	//zad 2 //
+	//	int liczba;
+	//	 
+	//	do
+	//	{
+	//		cout << " podaj liczbe :", cin >> liczba;
+	//	} while (liczba >= 0);
 
-	/*int main() {
-	float a, b, c, d, srednia;
-	std::cout << "Podaj liczbe a ";
-	std::cin >> a;
-		std::cout << "Podaj liczbe b ";
-		std::cin >> b;
-		std::cout << "Podaj liczbe c ";
-		std::cin >> c;
-		std::cout << "Podaj liczbe d ";
-		std::cin >> d;
-srednia = (a + b + c + d) / 4;
-std::cout << "Srednia arytmeyczna liczb to = " << srednia << endl;
-return 0;
-}
-*/
+	//	zad3 //
 
-	int a = 1, b = 12, wybor;
-	cout << "/n==============\n";
-	cout << "\n1.Suma\n2.Roznica\,3.iloczyn\n4. Iloraz";
-	cout << "wybierz dzialanie ", cin >> wybor;
-	switch (wybor)
-	{
-	case 1:
-		cout << "Suma :" << a << " + " << b << "=" << a + b << endl;
-		break;
-	case 2:
-		cout << "Roznica :" << a << " - " << b << " = " << a - b << endl;
-		break;
-	case 3:
-		cout << "Iloraz :" << a << " * " << b << " = " << a * b << endl;
-		break;
-	case 4:
-		cout << "Iloraz :" << a << " / " << b << " = " << a / b << endl;
-		break;
-	default:
-		cout << "Nie ma takiej opcji\n";
-		break;
-	}
-}
-//}
-//
-//
-//		
+
+	//int liczba, ile_d = 0, ile_u=0, suma_d = 0, suma_u = 0;
+
+	//for (int i = 0; i < 10; i++) {
+
+	//	cout << "podaj liczbe :", cin >> liczba;
+	//	if (liczba >= 0) {
+
+	//		ile_d++;
+	//		suma_d += liczba;
+	//	}
+	//	else {
+	//		ile_u++;
+	//		suma_u += liczba;
+
+	//	} //koniec petli 
+
+	//	cout << "suma liczb dodatnich : " << suma_d << ", ilość liczb dodatnich : " << ile_d << endl;
+	//	cout << "suma liczb ujemnych  : " << suma_d << ", ilość liczb ujemnych  : " << ile_d << endl;
+
+
+	//}
+
+	//zad 4//
+
+	///*int n, liczba, suma = 0;
+	//cout << "podaj n: ", cin >> n;
+
+	//if (n > 0) {
+	//	for (int i = 0; i < n; i++) {
+	//		cout << "podaj  liczbe : ", cin >> liczba;
+	//		if (liczba % 2 == 0) suma += liczba;
+	//	}
+	//	cout << "suma liczb parzystych : " << suma << endl;
+	//}
+	//else cout << "n nie moze byc <\ n";
+
+
+
+
+	//zad 5 //
+	//int  liczba;
+	//srand(time(null));
+	//for (int i = 0; i < 5; i++) {
+	//	liczba = ;
+	//	rand ()  %11 - 5; // -5 ,5
+	//	rand () % 9 + 1; // 1 , 10 
+	//	rand () % 11 ;     // 0 , 10
+	// }
+
+
+
+	// zad 1 //
+	//{
+	//	int i{ 1 }, n, sumaP{ 0 };
+	//	cout << "Podaj liczbe studentow: ", cin >> n;
+	//	while (n < 1 || n>100) cout << "Podaj WLASCIWA liczbe studentow: ", cin >> n;
+
+	//	while (i <= n) {
+	//		int punkt;
+	//		cin >> punkt;
+	//		if (punkt >= 0 && punkt <= 100)
+	//			sumaP += punkt;
+	//		else continue;
+	//		i++;
+
+	//	}
+	//	cout << "Srednia punktow w grupie = " << (float)sumaP / n << endl;
+	//	//NIESKOŃCZONA PĘTLA//
+	//		while (true) {
+	//			int punkt;
+	//			cin >> punkt;
+	//			if (punkt >= 0 && punkt <= 100)
+	//				sumaP += punkt;
+	//			else continue;
+	//			i++;
+	//		}
+	//		cout << "Srednia punktow w grupie = " << (float)sumaP / n << endl;
+	//	//PĘTLA DO_WHILE //
+	//		sumaP = 0;
+	//		 do{
+	//			int punkt;
+	//			cin >> punkt;
+	//			if (punkt >= 0 && punkt <= 100)
+	//				sumaP += punkt;
+	//			else continue;
+	//			i++;
+	//		 } while (true);
+	//		cout << "Srednia punktow w grupie = " << (float)sumaP / n << endl;
+	//	*/
+	//	// zad 5 //
+	//	for (int j = 1; j <= 100; j++)	cout << j << " ";
+	//	cout << "\n\n";
+	//	for (int j = 100; j >= 0; j--)	cout << j << " ";
+	//	cout << "\n\n";
+	//	for (int j = 7; j <= 77; j += 7)	cout << j << " ";
+	//	cout << "\n\n";
+	//	for (int j = 20; j >= 0; j -= 2)	cout << j << " ";
+	//	cout << "\n\n";
+
+	//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
